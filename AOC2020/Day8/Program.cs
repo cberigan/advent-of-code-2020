9e935baa126c2be0cf5a4391a46063fe6631986b
Corey Berigan <cberigan1@gmail.com>
2020-12-09 00:00:07 -0600

    day 9

---
+++ AOC2020/Day8/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            Part1(lines);
+            Part2(lines);
+            Console.ReadLine();
+
+        }
+
+        static void Part2(string[] lines)
+        {
+            var ops = new HashSet<int>();
+
+            for (int j = 0; j < lines.Length; j++)
+            {
+                var op = lines[j].Split(' ')[0];
+                if (op == "nop" || op == "jmp") ops.Add(j);
+            }
+            
+            foreach (var opIndex in ops)
+            {
+                var set = new HashSet<int>();
+                var acc = 0;
+                var i = 0;
+                var infLoopFound = false;
+                while (i < lines.Length)
+                {
+                    var elem = lines[i].Split(' ');
+
+                    string op = elem[0];
+                    if(i == opIndex)
+                    {
+                        op = elem[0] == "nop" ? "jmp" : "nop";
+                    }
+                    var num = int.Parse(elem[1]);
+
+                    if (set.Contains(i))
+                    {
+                        infLoopFound = true;
+                        break;
+                    }
+                    set.Add(i);
+
+                    if (op == "acc") acc += num;
+
+                    var jump = op == "jmp" ? num : 1;
+                    i += jump;
+
+                }
+                if (infLoopFound) continue;
+                else
+                {
+                    Console.WriteLine(acc);
+                    return;
+                }
+
+
+            }
+
+            
+        }
+
+
+        static void Part1(string[] lines)
+        {
+            var set = new HashSet<int>();
+            var acc = 0;
+            var i = 0;
+            while (true)
+            {
+                var elem = lines[i].Split(' ');
+                var op = elem[0];
+                var num = int.Parse(elem[1]);
+                if (set.Contains(i)) break;
+                set.Add(i);
+
+                if (op == "acc") acc += num;
+
+                var jump = op == "jmp" ? num : 1;
+                i += jump;
+
+            }
+
+            Console.WriteLine(acc);
+        }
+
+        static string input = @"";
+    }
+}
---
+++ day9.py
@@ -0,0 +1,7 @@
+import math
+def part1():
+    seats = set()
+    f = open("input9.txt", "r")
+    for x in f:
+        x = x.strip()
+        
